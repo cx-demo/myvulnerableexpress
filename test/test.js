f8e797704aa75bbe4210b491284b93d4972abbe1
Pedric kng <pedrickng@microsoft.com>
2026-08-18 09:05:23 +0800

    Add .gitignore file and update test configuration for CI reporting

--- test/test.js
+++ test/test.js
@@ -2,8 +2,18 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('App', function() {
+  let server;
+
+  before(function(done) {
+    server = app.listen(0, '127.0.0.1', done);
+  });
+
+  after(function(done) {
+    server.close(done);
+  });
+
   it('has the default page', function(done) {
-    request(app)
+    request(server)
       .get('/')
       .expect(/Welcome to Express/, done);
   });
